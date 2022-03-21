1f915bda93988e710d851134209f693e7d51a102
Krymov Andrey <black_lord_1999@mail.ru>
2022-03-21 20:38:43 +0700

    Project X
    Рабочая версия тестового задания

--- My project (1)/Assets/Scripts/ChangeData.cs
+++ My project (1)/Assets/Scripts/ChangeData.cs
@@ -18,9 +18,9 @@ public class ChangeData : MonoBehaviour
     }
     public void Change()
     {
-        cube.distance = float.Parse(distance.text);
-        cube.speed = float.Parse(speed.text);
-        spawn.time = float.Parse(time.text);
+        cube.distance = float.Parse(distance.text, System.Globalization.CultureInfo.GetCultureInfo("en-US"));
+        cube.speed = float.Parse(speed.text, System.Globalization.CultureInfo.GetCultureInfo("en-US"));
+        spawn.time = float.Parse(time.text, System.Globalization.CultureInfo.GetCultureInfo("en-US"));
 
         output.text = $"Текущие данные: \r\n\tРасстояние: {cube.distance}\r\n\tСкорость: {cube.speed}\r\n\tВремя: {spawn.time}";
     }
